d5e73abbfcada7d48ac079f6bf29e645b4049b62
CanoBroock <canobroock@gmail.com>
2022-12-14 19:23:00 +0100

    bug fixed

--- src/main/java/cat/itacademy/barcelonactiva/CanoBroock/Cesar/s05/t01/n03/controllers/FlorController.java
+++ src/main/java/cat/itacademy/barcelonactiva/CanoBroock/Cesar/s05/t01/n03/controllers/FlorController.java
@@ -2,9 +2,6 @@ package cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.controllers;
 
 import cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.model.domain.FlorEntity;
 import cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.model.dto.FlorDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,40 +12,55 @@ import java.util.List;
 public class FlorController {
 
     private WebClient webClient;
-    public FlorController(WebClient webClient){this.webClient = webClient;}
 
-    /*@PostMapping("/clientFlorsAdd")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void anadirFlor(@RequestBody FlorEntity florEntity) {
-
+    public FlorController(WebClient webClient) {
+        this.webClient = webClient;
     }
 
-    @PutMapping("/clientFlorsUpdate/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateFlor(@PathVariable("id") long id, @RequestBody FlorEntity florEntity) {
-
-    }
-
-    @DeleteMapping("/clientFlorsDelete/{id}")
-    public ResponseEntity<HttpStatus> deleteFlor(@PathVariable("id") int id) {
-
-    }*/
-
-    @GetMapping("/clientFlorsGetOne/{id}")
-    public FlorEntity getFlorById(@PathVariable("id") int id){
-        return webClient.get().uri("http://localhost:9001/getOne/",id)
+    //FUNCIONA
+    @PostMapping("/clientFlorsAdd")
+    public FlorEntity anadirFlor(@RequestBody FlorEntity florEntity) {
+        return webClient.post().uri("http://localhost:9001/flor/add")
+                .syncBody(florEntity)
                 .retrieve()
                 .bodyToMono(FlorEntity.class)
                 .block();
     }
+    /*
+    @PutMapping("/clientFlorsUpdate/{id}")
+    public void updateFlor(@PathVariable("id") int idFlor, @RequestBody FlorEntity florEntity) {
+        webClient.put().uri("http://localhost:9001/update/{id}", idFlor)
+                .syncBody(florEntity)
+                .retrieve()
+                .bodyToMono(FlorEntity.class)
+                .block();
+    }*/
 
+    //FUNCIONA
+    @DeleteMapping("/clientFlorsDelete/{id}")
+    public String deleteFlor(@PathVariable("id") int idFlor) {
+        return webClient.delete().uri("http://localhost:9001/flor/delete/{id}", idFlor)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+
+        @GetMapping("/clientFlorsGetOne/{id}")
+        public FlorEntity getFlorById(@PathVariable("id") int idFlor){
+            return webClient.get().uri("http://localhost:9001/getOne/{id}",idFlor)
+                    .retrieve()
+                    .bodyToMono(FlorEntity.class)
+                    .block();
+        }
+    //FUNCIONA
     @GetMapping("/clientFlorsAll")
     public List<FlorDTO> getAllFlores() {
-    return webClient.get().uri("http://localhost:9001/flor/getAll")
-            .retrieve()
-            .bodyToFlux(FlorDTO.class)
-            .collectList()
-            .block();
+        return webClient.get().uri("http://localhost:9001/flor/getAll")
+                .retrieve()
+                .bodyToFlux(FlorDTO.class)
+                .collectList()
+                .block();
     }
 }
 
