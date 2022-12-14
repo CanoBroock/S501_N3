fab80257b77debf23a07de1a35eb6116517a8486
CanoBroock <canobroock@gmail.com>
2022-12-14 17:09:41 +0100

    bug fixed

---
+++ src/main/java/cat/itacademy/barcelonactiva/CanoBroock/Cesar/s05/t01/n03/configuration/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+class SwaggerConfig{
+    @Bean
+    WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+}
+
+
+/*@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("cat.itacademy.barcelonactiva.fernandezVidal.alejandro.s05.t01.n03.S05T01N03FernandezVidalAlejandro.controllers")).paths(PathSelectors.any()).build();
+    }
+}*/
--- src/main/java/cat/itacademy/barcelonactiva/CanoBroock/Cesar/s05/t01/n03/controllers/FlorController.java
+++ src/main/java/cat/itacademy/barcelonactiva/CanoBroock/Cesar/s05/t01/n03/controllers/FlorController.java
@@ -2,23 +2,22 @@ package cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.controllers;
 
 import cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.model.domain.FlorEntity;
 import cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.model.dto.FlorDTO;
-import cat.itacademy.barcelonactiva.CanoBroock.Cesar.s05.t01.n03.model.services.FlorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/flor")
 public class FlorController {
 
-    @Autowired
-    private FlorServiceImpl florServiceImpl;
+    private WebClient webClient;
+    public FlorController(WebClient webClient){this.webClient = webClient;}
 
-    @PostMapping("/clientFlorsAdd")
+    /*@PostMapping("/clientFlorsAdd")
     @ResponseStatus(HttpStatus.CREATED)
     public void anadirFlor(@RequestBody FlorEntity florEntity) {
 
@@ -32,20 +31,24 @@ public class FlorController {
 
     @DeleteMapping("/clientFlorsDelete/{id}")
     public ResponseEntity<HttpStatus> deleteFlor(@PathVariable("id") int id) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 
-    /*@GetMapping("/clientFlorsGetOne/{id}")
-    public ResponseEntity<FlorEntity> getFlorById(@PathVariable("id") int id) {
-        Optional<FlorEntity> florData = florRepository.findById((long) id);
-
-        if (florData.isPresent()) {
-            return new ResponseEntity<>(florData.get(), HttpStatus.OK);
     }*/
 
+    @GetMapping("/clientFlorsGetOne/{id}")
+    public FlorEntity getFlorById(@PathVariable("id") int id){
+        return webClient.get().uri("http://localhost:9001/getOne/",id)
+                .retrieve()
+                .bodyToMono(FlorEntity.class)
+                .block();
+    }
+
     @GetMapping("/clientFlorsAll")
-    public ResponseEntity<List<FlorDTO>> getAllFlores() {
-        return new ResponseEntity<>(florServiceImpl.getAllFlores(), HttpStatus.OK);
+    public List<FlorDTO> getAllFlores() {
+    return webClient.get().uri("http://localhost:9001/flor/getAll")
+            .retrieve()
+            .bodyToFlux(FlorDTO.class)
+            .collectList()
+            .block();
     }
 }
 
